edf4947a4f209218a33f63bec5a848949f098a2a
Danny Callaghan <dannycallaghan@gmail.com>
2021-01-28 23:03:54 +0000

    Just need to tidy and comment

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let imgs = []; // Store all face images
 let avgImg; // Where we'll generate the average face
 const numOfImages = 30; // Number of facees used
-
+let currentImage;// Stores the current image so that a 'random' image is never one already being displayed
+let imgAverages = []; // Store the average channels for each image
 /**
  * P5 preload functionality
  *
@@ -27,6 +28,12 @@ function setup() {
 
   // Create average image
   avgImg = createGraphics(firstImgWidth, firstImgHeight);
+
+  // Call the loadPixels function on relevant elements
+  doLoadPixels();
+
+  currentImage = getRandomImage();
+  
 }
 
 /**
@@ -38,19 +45,17 @@ function draw() {
   background(125);
 
   // Draw first (left) image
-  drawImage();
-
-  // Call the loadPixels function on relevant elements
-  doLoadPixels();
+  drawImage(currentImage);
 
   // Set the average image to red (gets overwritten later)
-  setAverageImageToRed();
+  // Commented out as it's not necessary for the further steps
+  // setAverageImageToRed();
 
   // Generates the average face image
   generateAverageFace();
 
   // Intensive, so don't loop
-  noLoop();
+  //noLoop();
 }
 
 /**
@@ -58,21 +63,38 @@ function draw() {
  *
  * @return void.
  */
-function drawImage () {
-  image(imgs[0], 0, 0);
+function drawImage (index) {
+  image(imgs[index], 0, 0);
 }
 
-/**
- * Call the loadPixels function on relevant elements
- *
- * @return void.
- */
 function doLoadPixels () {
+  let sumR = 0;
+  let sumG = 0;
+  let sumB = 0;
+
+  const getSums = (img) => {
+    img.loadPixels();
+    const allPixels = img.width * img.height;
+    for (let i = 0, j = allPixels; i < j; i++) {
+      const x = floor(i / img.width); // X pixels
+      const y = i % img.height; // Y pixels
+      const index = ((img.width * y) + x) * 4; // Get pixel array
+      imgAverages[index] = imgAverages[index] ? imgAverages[index] + img.pixels[index] : img.pixels[index]; 
+      imgAverages[index + 1]= imgAverages[index + 1] ? imgAverages[index + 1] + img.pixels[index + 1] : img.pixels[index + 1]; 
+      imgAverages[index + 2] = imgAverages[index + 2] ? imgAverages[index + 2] + img.pixels[index + 2] : img.pixels[index + 2]; 
+      imgAverages[index + 3] = imgAverages[index + 3] ? imgAverages[index + 3] + img.pixels[index + 3] : img.pixels[index + 3]; 
+    }
+  }
+
   for (let i = 0, j = imgs.length; i < j; i++) {
     imgs[i].loadPixels();
+    getSums(imgs[i]);
   }
+
   avgImg.loadPixels();
 }
+
+
 
 /**
  * Set the average image to red
@@ -90,7 +112,7 @@ function setAverageImageToRed () {
       avgImg.pixels[index + 3] = 255;
     }
   }
-  // Update average image
+  // Update average imageqsp
   avgImg.updatePixels();
   image(avgImg, imgs[0].width, 0);
 }
@@ -101,26 +123,78 @@ function setAverageImageToRed () {
  * @return void.
  */
 function generateAverageFace () {
-  for (let i = 0; i < imgs[0].width; i++) {
-    for (let j = 0; j < imgs[0].height; j++) {
-      let index = ((imgs[0].width * j) + i) * 4;
-      let sumR = 0;
-      let sumG = 0;
-      let sumB = 0;
-      // Find the channel sum for each pixel of each image
-      for (let k = 0; k < imgs.length; k++) {
-        sumR += imgs[k].pixels[index + 0];
-        sumG += imgs[k].pixels[index + 1];
-        sumB += imgs[k].pixels[index + 2];
-      }
-      // Add the average of the channels to the channels of the avImg
-      avgImg.pixels[index + 0] = sumR / imgs.length;
-      avgImg.pixels[index + 1] = sumG / imgs.length;
-      avgImg.pixels[index + 2] = sumB / imgs.length;
-      avgImg.pixels[index + 3] = 255;
-    }
+
+  let amount = 0;
+
+  if (
+    (mouseY > 0 && mouseY < height) &&
+    (mouseX > width / 2)
+  ) {
+    amount = constrain(mouseX - avgImg.width, 0, avgImg.width);
   }
+  
+  //const amount = avgImg.width;
+  
+  //console.log(amount);
+
+  const allPixels = imgs[0].width * imgs[0].height;
+  for (let i = 0, j = allPixels; i < j; i++) {
+    const x = floor(i / imgs[0].width); // X pixels
+    const y = i % imgs[0].height; // Y pixels
+    const index = ((imgs[0].width * y) + x) * 4; // Get pixel array
+
+
+    avgImg.pixels[index + 0] = lerp(imgs[currentImage].pixels[index], imgAverages[index] / imgs.length, amount * (1 / avgImg.width));
+    avgImg.pixels[index + 1] = lerp(imgs[currentImage].pixels[index + 1], imgAverages[index + 1] / imgs.length, amount * (1 / avgImg.width));
+    avgImg.pixels[index + 2] = lerp(imgs[currentImage].pixels[index + 2], imgAverages[index + 2] / imgs.length, amount * (1 / avgImg.width));
+    avgImg.pixels[index + 3] = 255;
+  }
+
+
+
+  //   avgImg.pixels[index + 0] = imgAverages[index] / imgs.length;
+  //   avgImg.pixels[index + 1] = imgAverages[index + 1] / imgs.length;
+  //   avgImg.pixels[index + 2] = imgAverages[index + 2] / imgs.length;
+  //   avgImg.pixels[index + 3] = 255;
+  // }
+
   // Update average image
   avgImg.updatePixels();
   image(avgImg, imgs[0].width, 0);
 }
+
+/**
+ * P5 event listener - key presses
+ *
+ * @return void.
+ */
+function getRandomImage () {
+  const randomImage = round(random(0, numOfImages - 1));
+  if (currentImage && currentImage === randomImage) {
+    return getRandomImage();
+  }
+  currentImage = randomImage;
+  return randomImage;
+}
+
+/**
+ * P5 event listener - key presses
+ *
+ * @return void.
+ */
+function keyPressed () {
+  drawImage(getRandomImage());
+}
+
+// function _drawImage () {
+//   if (
+//     (mouseY > 0 && mouseY < height) &&
+//     (mouseX > width / 2 && mouseX < width)
+//   ) {
+//     generateAverageFace();
+//   }
+// }
+
+// function mouseMoved () {
+//   _drawImage();
+// }
